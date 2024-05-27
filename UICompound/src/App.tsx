69bca9dcca029016f3f4c8dba4850d2e2709d9c7
Asharp19 <rishabh.3172000@gmail.com>
2024-05-27 12:45:49 +0300

    UICompound

--- UICompound/src/App.tsx
+++ UICompound/src/App.tsx
@@ -11,6 +11,7 @@ const ReturnPromise = () => {
   const [provider, setProvider] = useState<JSONRPC>();
   const [signer, setSigner] = useState<ethers.Signer>();
   const [contract, setContract] = useState<ethers.Contract>();
+
   useEffect(() => {
     const providerDetails = async () => {
       const provider1 = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
@@ -19,14 +20,46 @@ const ReturnPromise = () => {
       const signer2 = await provider1.getSigner(3);
 
       const contractPool = new ethers.Contract(
-        "0xec9eaEbE247aAbEC55B87C474760a8af74c3d9EA",
+        "0xF837C675e5122D68a2B8bDfc4e50C59118Bd417E",
         abi.abi,
         signer1
       );
-      // ethers;
+
+      const contractFaucet = new ethers.Contract(
+        "0x68793eA49297eB75DFB4610B68e076D2A5c7646C",
+        abi.abi2,
+        signer1
+      );
+
+      const contractCompound = new ethers.Contract(
+        "0xA6c8D1c55951e8AC44a0EaA959Be5Fd21cc07531",
+        abi.abi3,
+        signer1
+      );
+
+      // await contractFaucet.drip("0xa6c8d1c55951e8ac44a0eaa959be5fd21cc07531");
+      const compBalance: number = await contractCompound.balanceOf(
+        signer1.address
+      );
+
+      console.log(Number(compBalance));
+      // await contractCompound.transfer(
+      //   await contractPool.getAddress(),
+      //   Number(compBalance) * 0.5
+      // );
+      // ethers;1
       const balanceValue = await contractPool.BalanceCheck();
 
-      console.log(ethers.toBigInt(balanceValue.data), signer1, signer2);
+      // await contractPool.supplyCollateral({ value: 100000000 });
+      // const isBorrowAllowed = await contractPool.isBorrowAllowed();
+      // const isLiquidatable = await contractPool.isLiquidatable();
+      // console.log(
+      //   ethers.toBigInt(balanceValue.data),
+      //   signer1,
+      //   signer2,
+      //   isBorrowAllowed,
+      //   isLiquidatable
+      // );
 
       setProvider(provider1);
       setSigner(signer1);
